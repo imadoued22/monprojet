a1e70413daf3c6a85a9ca7bd185cd811803a2586
EL ALAM Nassim <nassim.el.alam@octo.ma>
2023-05-11 11:04:59 +0100

    refactor: move package security inside config

--- src/main/java/com/motNotification/moteur/config/security/SecurityConfig.java
+++ src/main/java/com/motNotification/moteur/config/security/SecurityConfig.java
@@ -1,12 +1,10 @@
-package security;
+package com.motNotification.moteur.config.security;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +14,6 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @KeycloakConfiguration
 @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
--- src/main/java/com/motNotification/moteur/config/security/keycloakAdapter.java
+++ src/main/java/com/motNotification/moteur/config/security/keycloakAdapter.java
@@ -1,4 +1,4 @@
-package security;
+package com.motNotification.moteur.config.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.context.annotation.Bean;
